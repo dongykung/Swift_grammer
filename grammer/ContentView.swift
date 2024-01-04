c0a285b81ff3a5ab269bd7f1bde749a19d066abc
dongkyung <kdkd422@naver.com>
2024-01-04 21:51:42 +0900

    Initial Commit

---
+++ grammer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  grammer
+//
+//  Created by 김동경 on 2024/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ grammer/grammerApp.swift
@@ -0,0 +1,17 @@
+//
+//  grammerApp.swift
+//  grammer
+//
+//  Created by 김동경 on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct grammerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
