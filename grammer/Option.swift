9eb38c90901e5faf74d2750724b6109cb612926d
dongkyung <kdkd422@naver.com>
2024-01-09 17:30:50 +0900

    optional, sample code

---
+++ grammer/Option.swift
@@ -0,0 +1,18 @@
+//
+//  Option.swift
+//  grammer
+//
+//  Created by 김동경 on 2024/01/09.
+//
+
+import SwiftUI
+
+struct Option: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Option()
+}
